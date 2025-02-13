526522550f2bbf66ec0419f624f6f7dc99e1f29c
Shankar <shankarb3061997@gmail.com>
2025-02-13 14:42:01 +0530

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Shankar Balasubramaniam on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
